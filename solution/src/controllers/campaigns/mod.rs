c99dbd512d6b1e12fa0246c445a037eb59de436a
CyberFather <maks12ate@gmail.com>
2025-02-12 23:38:09 +0300

    impl: campaign getter by id

--- solution/src/controllers/campaigns/mod.rs
+++ solution/src/controllers/campaigns/mod.rs
@@ -5,10 +5,11 @@ use axum::{
     routing::{get, post},
     Router,
 };
-use routes::{create, list};
+use routes::{create, get_campaign_by_id, list};
 
 pub fn get_routes() -> Router<AppState> {
     Router::new()
         .route("/campaigns", post(create))
         .route("/campaigns", get(list))
+        .route("/campaigns/{compaign_id}", get(get_campaign_by_id))
 }
--- solution/src/controllers/campaigns/routes.rs
+++ solution/src/controllers/campaigns/routes.rs
@@ -83,10 +83,49 @@ pub async fn list(
     .await
     .map_err(ProdError::DatabaseError)?;
 
+    if rows.is_empty() {
+        return Err(ProdError::NotFound(format!(
+            "No campaigns were found for advertiser - `{:?}`",
+            advertiser_id
+        ))
+        .into());
+    }
+
     let campaigns = paginate(rows, query.size, query.page)
         .into_iter()
         .map(CampaignModel::from)
         .collect();
 
     Ok(Json(campaigns))
+}
+
+pub async fn get_campaign_by_id(
+    State(state): State<AppState>,
+    Path((advertiser_id, campaign_id)): Path<(Uuid, Uuid)>,
+) -> Result<Json<CampaignModel>, Response<String>> {
+    let mut conn = state.pool.conn().await?;
+
+    let campaign = sqlx::query_as!(
+        CampaignRow,
+        r#"
+        SELECT id, advertiser_id, impressions_limit, clicks_limit, cost_per_impression,
+               cost_per_click, ad_title, ad_text, start_date, end_date,
+               gender as "gender: GenderModel", age_from, age_to, location
+        FROM campaigns
+        WHERE advertiser_id = $1 AND id = $2
+        "#,
+        advertiser_id,
+        campaign_id
+    )
+    .fetch_one(&mut *conn)
+    .await
+    .map_err(|err| match err {
+        sqlx::Error::RowNotFound => ProdError::NotFound(format!(
+            "No campaign was found with id - `{:?}` for advertiser - `{:?}`",
+            campaign_id, advertiser_id
+        )),
+        _ => ProdError::DatabaseError(err),
+    })?;
+
+    Ok(Json(campaign.into()))
 }
