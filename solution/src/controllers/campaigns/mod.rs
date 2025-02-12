6a6c3290bb8060e02cfd9c7e6f6a190fe6d50ee4
CyberFather <maks12ate@gmail.com>
2025-02-13 00:03:52 +0300

    impl: campaign deletion

--- solution/src/controllers/campaigns/mod.rs
+++ solution/src/controllers/campaigns/mod.rs
@@ -2,10 +2,10 @@ mod routes;
 
 use crate::AppState;
 use axum::{
-    routing::{get, post, put},
+    routing::{delete, get, post, put},
     Router,
 };
-use routes::{create, get_campaign_by_id, list, update};
+use routes::{create, delete_campaign, get_campaign_by_id, list, update};
 
 pub fn get_routes() -> Router<AppState> {
     Router::new()
@@ -13,4 +13,5 @@ pub fn get_routes() -> Router<AppState> {
         .route("/campaigns", get(list))
         .route("/campaigns/{compaign_id}", get(get_campaign_by_id))
         .route("/campaigns/{compaign_id}", put(update))
+        .route("/campaigns/{compaign_id}", delete(delete_campaign))
 }
--- solution/src/controllers/campaigns/routes.rs
+++ solution/src/controllers/campaigns/routes.rs
@@ -185,3 +185,32 @@ pub async fn update(
 
     Ok(Json(campaign.into()))
 }
+
+pub async fn delete_campaign(
+    State(state): State<AppState>,
+    Path((advertiser_id, campaign_id)): Path<(Uuid, Uuid)>,
+) -> Result<StatusCode, Response<String>> {
+    let mut conn = state.pool.conn().await?;
+
+    let rows_affected = sqlx::query!(
+        r#"
+        DELETE FROM campaigns
+        WHERE advertiser_id = $1 AND id = $2
+        "#,
+        advertiser_id,
+        campaign_id
+    )
+    .execute(&mut *conn)
+    .await
+    .map_err(ProdError::DatabaseError)?;
+
+    if rows_affected.rows_affected() == 0 {
+        return Err(ProdError::NotFound(format!(
+            "No campaign was found with id - `{:?}` for advertiser - `{:?}`",
+            campaign_id, advertiser_id
+        ))
+        .into());
+    }
+
+    Ok(StatusCode::NO_CONTENT)
+}
